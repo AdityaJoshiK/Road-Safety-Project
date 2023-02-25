f0b1059e826e4ab2ed23835ddf5434acf1be4a79
HP <HP@LAPTOP-6KG6MJDL>
2023-02-26 02:04:06 +0530

    Kush-Hospital selectall

--- Controllers/MST_HospitalController.cs
+++ Controllers/MST_HospitalController.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Road_Safety_Project.Controllers
 {
     public class MST_HospitalController : Controller
     {
+        private IConfiguration Configuration;
+        public MST_HospitalController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        #region SelectAll
         public IActionResult Index()
         {
+            string str = Configuration.GetConnectionString("myConnectionString");
+            SqlConnection conn = new SqlConnection(str);
 
-            return View();
+            conn.Open();
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "PR_MST_Hospital_SelectAll";
+            DataTable dt = new DataTable();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+
+            return View("MST_HospitalList",dt);
+
+            conn.Close();
         }
+        #endregion
     }
 }
--- Models/MST_HospitalModel.cs
+++ Models/MST_HospitalModel.cs
@@ -9,9 +9,13 @@ namespace Road_Safety_Project.Models
         public string? HospitalName { get; set; }
         public string? HospitalAddress { get; set; }
         public int CountryID { get; set; }
+        public string? CountryName { get; set; }
         public int StateID { get; set; }
+        public string? StateName { get; set; }
         public int CiytID { get; set; }
+        public string? CityName { get; set; }
         public int AreaID { get; set; }
+        public string? AreaName { get; set; }
         public string? HospitalMobileNo { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime ModificationDate { get; set; }
